ed87b69a6ce4306340f21f959d127e71ec3fd615
Marc_S <117986477+MarcENTI@users.noreply.github.com>
2024-12-02 11:08:34 +0100

    Fragments

--- CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/MainActivity.kt
+++ CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/MainActivity.kt
@@ -1,16 +1,19 @@
 package firebase.companionPersona.enti24
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import firebase.companionPersona.enti24.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
