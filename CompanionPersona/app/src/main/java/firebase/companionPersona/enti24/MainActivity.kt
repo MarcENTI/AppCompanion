5a63bc396173c5d43d83d638778eca6ec09038d2
Marc_S <117986477+MarcENTI@users.noreply.github.com>
2024-12-17 10:53:46 +0100

    Revert "Merge branch 'feature/googleLogin' into develop"
    This reverts commit 594fa529fb31f0ba25c36a6fca9e82282dea4cc2, reversing
    changes made to ef409d582769e4de444f6b7ccc786409be05ef7b.

--- CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/MainActivity.kt
+++ CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/MainActivity.kt
@@ -1,44 +1,17 @@
 package firebase.companionPersona.enti24
 
-import android.app.ComponentCaller
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.SignInButton
-import com.google.android.gms.common.api.ApiException
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var googleSignInClient: GoogleSignInClient
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
         super.onCreate(savedInstanceState)
-         setContentView(R.layout.activity_login_google)
-
-        val CHORISOMYBELOVED = "431744884113-hgmcd9qdihi8uam2kc28osm3g0nboect.apps.googleusercontent.com"
-
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
-        requestIdToken(CHORISOMYBELOVED)
-            .requestEmail().build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-        googleSignInClient.signOut()
-
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-
-        account?.let{
-            Log.d("Login Google", "Ya he robado la informacion de este usuario antes: " + account.displayName)
-        }?:run {
-            findViewById<SignInButton>(R.id.btn_login_google).setOnClickListener{signIn()}
-        }
-
-
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
             val bottomNavFragment = BottomNavFragment()
@@ -48,31 +21,6 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
 
-    }
-
-    private fun signIn()
-    {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, 9001)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?, caller: ComponentCaller)
-    {
-        super.onActivityResult(requestCode, resultCode, data, caller)
-
-        if(requestCode == 9001)
-        {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            if(task.isSuccessful)
-            {
-                val account = task.getResult(ApiException::class.java)
-                Log.d("Login Google", "Tengo la info de: " + account.displayName)
-            }
-            else
-            {
-                Log.d("Login Google", "Error en el login: " + task.exception)
-            }
-        }
     }
 
 
