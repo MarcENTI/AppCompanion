a4bc6ae324e93f8dacb1e0f78aaa751d8043c6cc
Shiro <eric.maestre@enti.cat>
2024-12-30 19:10:35 +0100

    profile created
    doesn't work

--- CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/ProfileActivity.kt
+++ CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/ProfileActivity.kt
@@ -1,16 +1,72 @@
 package firebase.companionPersona.enti24
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var db: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
+
+        //iniciarlizar firestore y authentication
+        db = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        //textViews
+        val userNameTextView = findViewById<TextView>(R.id.user_name)
+        val userIdTextView = findViewById<TextView>(R.id.user_id)
+
+        val currentUser = auth.currentUser
+        if(currentUser != null)
+        {
+            val userId = currentUser.uid
+
+            //obtener datos de firestore
+            db.collection("users").document(userId).get()
+                .addOnSuccessListener {
+                    document->
+                    if(document !=null && document.exists())
+                    {
+                        val userName = document.getString("name") ?: "Nombre no encontrado"
+                        val userCustomId = document.getString("id") ?: "ID no encontrado"
+
+                        //act textViews
+                        userNameTextView.text = userName
+                        userIdTextView.text = userCustomId
+                    }else {
+                        userNameTextView.text = "Usuario no encontrado"
+                        userIdTextView.text = "N/A"
+                    }
+                }
+                .addOnFailureListener {
+                    e->
+                    userNameTextView.text = "Error al cargar"
+                    userIdTextView.text = "N/A"
+                }
+
+
+            
+        }
+        else
+        {
+            userNameTextView.text = "No has iniciado sesión"
+            userIdTextView.text = "N/A"
+        }
+
+
+
+
 
     }
 }
