ba24665604cf63323303cc41e61da8b589953cb3
Aleix <aleix.martinez@enti.cat>
2025-01-07 17:54:29 +0100

    api con imagenes

--- CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/PersonaAdapter.kt
+++ CompanionPersona/app/src/main/java/firebase/companionPersona/enti24/PersonaAdapter.kt
@@ -3,12 +3,13 @@ package firebase.companionPersona.enti24
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class PersonaAdapter(
-    private var personas: List<Persona>
-) : RecyclerView.Adapter<PersonaAdapter.PersonaViewHolder>() {
+class PersonaAdapter(private var personas: List<Persona>) :
+    RecyclerView.Adapter<PersonaAdapter.PersonaViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonaViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -20,7 +21,7 @@ class PersonaAdapter(
         holder.bind(personas[position])
     }
 
-    override fun getItemCount() = personas.size
+    override fun getItemCount(): Int = personas.size
 
     fun updateData(newPersonas: List<Persona>) {
         personas = newPersonas
@@ -31,12 +32,19 @@ class PersonaAdapter(
         private val arcanaText: TextView = itemView.findViewById(R.id.arcanaTextView)
         private val levelText: TextView = itemView.findViewById(R.id.personaLevelTextView)
         private val nameText: TextView = itemView.findViewById(R.id.personaNameTextView)
+        private val personaImageView: ImageView = itemView.findViewById(R.id.personaImageView) // ImageView para la imagen
 
         fun bind(persona: Persona) {
-
             arcanaText.text = persona.arcana
             levelText.text = persona.level.toString()
             nameText.text = persona.name
+
+            // Cargar la imagen desde la URL usando Glide (glide recomendado por chat gpt)
+            Glide.with(itemView.context)
+                .load(persona.image)
+
+                .into(personaImageView)
         }
     }
 }
+
